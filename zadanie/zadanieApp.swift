c1d0018c784697e644e8a9719516b6301f309a35
Peter Osusky <peterosusky94@gmail.com>
2023-07-10 11:07:49 +0200

    initial commit

---
+++ zadanie/zadanieApp.swift
@@ -0,0 +1,17 @@
+//
+//  zadanieApp.swift
+//  zadanie
+//
+//  Created by Peter on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct zadanieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
