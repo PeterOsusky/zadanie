fae81d3bcb11315fa063c8fcb25e12de3480382f
Peter Osusky <peterosusky94@gmail.com>
2023-07-10 14:56:34 +0200

    updated detailed product view

--- zadanie/ProductDetailView.swift
+++ zadanie/ProductDetailView.swift
@@ -12,11 +12,43 @@ struct ProductDetailView: View {
     @ObservedObject var productDetailVM: ProductDetailViewModel
 
     var body: some View {
-        VStack {
-            Text(productDetailVM.product.title)
-            Text(productDetailVM.product.description)
-            // Other details...
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+
+                AsyncImage(url: URL(string: productDetailVM.product.image)) { image in
+                    image.resizable()
+                } placeholder: {
+                    ProgressView()
+                }
+                .scaledToFit()
+                .frame(width: geometry.size.width * 0.5)
+
+                Spacer()
+
+                Text(productDetailVM.product.title)
+                    .font(.title)
+                    .foregroundColor(.blue)
+
+                Spacer()
+
+                Text(productDetailVM.product.description)
+                    .font(.body)
+
+                Spacer()
+
+                HStack {
+                    Text("Product ID: \(productDetailVM.product.id)")
+                    
+                    Spacer()
+                    
+                    Text("Price: \(productDetailVM.product.price)")
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .navigationBarTitle(productDetailVM.product.category, displayMode: .inline)
         }
     }
 }
-
